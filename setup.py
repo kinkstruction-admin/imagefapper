bd5bf0d2f8778da76b0e53aae98275dea7bbb97f
Kinkstruction Admin <admin@kinkstruction.com>
2015-03-21 00:58:50 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+from imagefapper import __version__
+
+try:
+    with open("README.rst") as f:
+        long_description = f.read()
+except IOError:
+    long_description = ""
+
+try:
+    with open("requirements.txt") as f:
+        requirements = [x for x in [y.strip() for y in f.readlines()] if x]
+except IOError:
+    requirements = []
+
+setuptools.setup(
+    install_requires=requirements,
+    version=__version__,
+    name="imagefapper",
+    license="MIT",
+    author="Jack Maney",
+    author_email="jackmaney@gmail.com",
+    packages=setuptools.find_packages(),
+    long_description=long_description
+)
